503112bd7feb5e326eef8866a0dcfd6d781b4f44
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-08-22 18:54:54 -0300

    Finalização do java script e estilização do game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 let isJumping = false;
-
+let position = 0;
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
         if (!isJumping) {
@@ -11,8 +12,7 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
-
+    
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -36,5 +36,35 @@ function jump() {
         }
     }, 20);
 }
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+    
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if ( cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            // game over
+            clearInterval(leftInterval);
+            document.querySelector('.background').innerHTML = '<h1 class="game-over">Fim de jogo!</h1><button class="reiniciar" onClick="window.location.reload()">REINICIAR</button>';
+
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition +'px';     
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+
+}
+
+createCactus();
 
 document.addEventListener('keyup', handleKeyUp);
